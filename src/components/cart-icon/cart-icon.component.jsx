711f2f2a8bc6c84c4b51ee27bc878841af2bebfa
AyubBodirkhonov <apple@Ayubs-MacBookAir.local>
2023-01-06 21:21:07 +0900

    Added addproduct cart to cart context and write some logic

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,11 +1,10 @@
-import { useContext } from "react";
-import { CartContext } from "../../contexts/cart.context";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.style.scss";
-
+import { CartContext } from "../../contexts/cart.context";
+import { useContext } from "react";
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 const CartIcon = () => {
-  const {isCartOpen, setIsCartOpen } = useContext(CartContext);
-  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
 import { ProductsProvider } from "./contexts/products.context";
-import { CartProvider } from "./contexts/cart.context";
+
 import "./index.scss";
+import { CartProvider } from "./contexts/cart.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
